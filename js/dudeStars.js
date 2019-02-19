4a70807f3fd366a36c0006fdcd78153ae18a7aa0
diguifi <lumia-diguifi@hotmail.com>
2019-02-19 08:57:05 -0300

    Add ratings verifier for Dudes Quest

---
+++ js/dudeStars.js
@@ -0,0 +1,19 @@
+let xhttpGet = new XMLHttpRequest();
+xhttpGet.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+        let notas = JSON.parse(this.responseText).result.notas;
+        console.log(notas);
+
+        let somaNotas = 0;
+        notas.forEach(nota => {
+            somaNotas += nota;
+        });
+
+        let total = (somaNotas / notas.length).toFixed(1);
+
+        document.getElementById("ratings").innerHTML = total.toString() + '/5';
+    }
+};
+
+xhttpGet.open('GET', 'https://www.jsonstore.io/16824c0f7a1696d7acfabe21392bd07d8afcc0ba76cb366ab189dfa94186dc08', true);
+xhttpGet.send();
